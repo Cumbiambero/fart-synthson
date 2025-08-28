c6eaa51134cc6473010eb4ebf26895c3bab10d4c
Eduardo Hahn <eduardo.hahn@sbb.ch>
2025-08-28 16:36:25 +0200

    Improved sound

--- src/fart_simpson.cpp
+++ src/fart_simpson.cpp
@@ -55,32 +55,59 @@ void FartSimpson::process(const ProcessArgs& args) {
     float shameGain = 1.f - shameFinal;
     shameGain *= shameGain;
 
+    static float ampLFO = 0.f, ampLFOPhase = 0.f;
+    static float pitchLFO = 0.f, pitchLFOPhase = 0.f;
+    static float brownNoise = 0.f;
+    static float bubbleEnv = 0.f, bubblePhase = 0.f;
+
     if (voice.active) {
         voice.t += sampleRate;
 
         float baseFreq = 120.f - (ageFinal / 100.f) * 70.f;
-        float attack = 0.02f + (1.f - urgencyFinal) * 0.08f;
-        float decay = 0.25f + (1.f - urgencyFinal) * 0.3f;
+        float attack = 0.08f + (1.f - urgencyFinal) * 0.15f;
+        float decay = 0.5f + (1.f - urgencyFinal) * 0.5f;
         float env = 0.f;
         if (voice.t < attack) {
-            env = (voice.t / attack) * (0.7f + urgencyFinal * 0.6f);
+            env = (voice.t / attack) * (0.5f + urgencyFinal * 0.5f);
         } else {
             env = std::exp(-(voice.t - attack) / decay);
         }
         voice.env = env;
 
-        float wideLFO = std::sin(2.f * (float)M_PI * voice.t * (0.5f + retentionFinal * 4.0f));
-        float narrowLFO = std::sin(2.f * (float)M_PI * voice.t * (8.f + foodFinal * 32.f));
+        ampLFOPhase += sampleRate * (0.2f + foodFinal * 0.3f);
+        if (ampLFOPhase > 1.f) ampLFOPhase -= 1.f;
+        ampLFO = 0.7f + 0.3f * std::sin(2.f * (float)M_PI * ampLFOPhase + randomUniform() * 0.2f);
 
-        float pitch = baseFreq * (1.f + wideLFO * retentionFinal * 0.2f + narrowLFO * foodFinal * 0.05f);
+        pitchLFOPhase += sampleRate * (0.12f + retentionFinal * 0.18f);
+        if (pitchLFOPhase > 1.f) pitchLFOPhase -= 1.f;
+        pitchLFO = 1.f + 0.08f * std::sin(2.f * (float)M_PI * pitchLFOPhase + randomUniform() * 0.3f);
+
+        float wideLFO = std::sin(2.f * (float)M_PI * voice.t * (0.3f + retentionFinal * 2.0f));
+        float narrowLFO = std::sin(2.f * (float)M_PI * voice.t * (3.f + foodFinal * 10.f));
+        float pitch = baseFreq * pitchLFO * (1.f + wideLFO * retentionFinal * 0.25f + narrowLFO * foodFinal * 0.08f);
         voice.phase += pitch * sampleRate;
         if (voice.phase > 1.f) voice.phase -= 1.f;
 
         float core = std::tanh(std::sin(2.f * (float)M_PI * voice.phase));
-        float noise = (randomUniform() * 2.f - 1.f) * (0.1f + surpriseFinal * 0.6f);
-        float wet = std::sin(2.f * (float)M_PI * voice.phase * (2.5f + foodFinal * 2.f)) * (0.1f + foodFinal * 0.5f);
 
-        out = (core + noise + wet) * env * shameGain * 5.f;
+        float white = (randomUniform() * 2.f - 1.f);
+        brownNoise = (brownNoise + 0.02f * white) * 0.98f;
+        float noise = brownNoise * (0.18f + surpriseFinal * 0.7f);
+
+        float wet = std::sin(2.f * (float)M_PI * voice.phase * (1.5f + foodFinal * 2.5f)) * (0.12f + foodFinal * 0.6f);
+
+        if (bubbleEnv < 0.001f && randomUniform() < (0.008f + foodFinal * 0.03f + surpriseFinal * 0.01f)) {
+            bubbleEnv = 1.f;
+            bubblePhase = 0.f;
+        }
+        if (bubbleEnv > 0.001f) {
+            bubblePhase += sampleRate * (8.f + randomUniform() * 8.f);
+            float bubble = std::sin(2.f * (float)M_PI * bubblePhase) * bubbleEnv * (0.2f + foodFinal * 0.5f);
+            noise += bubble;
+            bubbleEnv *= 0.93f - 0.04f * surpriseFinal;
+        }
+
+        out = (core + noise + wet) * env * shameGain * ampLFO * 5.f;
 
         if (env < 0.001f) {
             voice.active = false;
@@ -109,7 +136,6 @@ void FartSimpson::readInputs(float &ageCV, float &urgencyCV, float &retentionCV,
         getParamQuantity(AGE_PARAM)->setDisplayValue(mapped);
         ageCV = (mapped - params[AGE_PARAM].getValue()) / 100.f;
     } else {
-        getParamQuantity(AGE_PARAM)->setDisplayValue(params[AGE_PARAM].getValue());
         ageCV = 0.f;
     }
     if (inputs[URGENCY_CV_INPUT].isConnected()) {
@@ -118,7 +144,6 @@ void FartSimpson::readInputs(float &ageCV, float &urgencyCV, float &retentionCV,
         getParamQuantity(URGENCY_PARAM)->setDisplayValue(mapped);
         urgencyCV = mapped - params[URGENCY_PARAM].getValue();
     } else {
-        getParamQuantity(URGENCY_PARAM)->setDisplayValue(params[URGENCY_PARAM].getValue());
         urgencyCV = 0.f;
     }
     if (inputs[RETENTION_CV_INPUT].isConnected()) {
@@ -127,7 +152,6 @@ void FartSimpson::readInputs(float &ageCV, float &urgencyCV, float &retentionCV,
         getParamQuantity(RETENTION_PARAM)->setDisplayValue(mapped);
         retentionCV = mapped - params[RETENTION_PARAM].getValue();
     } else {
-        getParamQuantity(RETENTION_PARAM)->setDisplayValue(params[RETENTION_PARAM].getValue());
         retentionCV = 0.f;
     }
     if (inputs[SHAME_CV_INPUT].isConnected()) {
@@ -136,7 +160,6 @@ void FartSimpson::readInputs(float &ageCV, float &urgencyCV, float &retentionCV,
         getParamQuantity(SHAME_PARAM)->setDisplayValue(mapped);
         shameCV = mapped - params[SHAME_PARAM].getValue();
     } else {
-        getParamQuantity(SHAME_PARAM)->setDisplayValue(params[SHAME_PARAM].getValue());
         shameCV = 0.f;
     }
     if (inputs[SURPRISE_CV_INPUT].isConnected()) {
@@ -145,7 +168,6 @@ void FartSimpson::readInputs(float &ageCV, float &urgencyCV, float &retentionCV,
         getParamQuantity(SURPRISE_PARAM)->setDisplayValue(mapped);
         surpriseCV = mapped - params[SURPRISE_PARAM].getValue();
     } else {
-        getParamQuantity(SURPRISE_PARAM)->setDisplayValue(params[SURPRISE_PARAM].getValue());
         surpriseCV = 0.f;
     }
     if (inputs[FOOD_CV_INPUT].isConnected()) {
@@ -154,7 +176,6 @@ void FartSimpson::readInputs(float &ageCV, float &urgencyCV, float &retentionCV,
         getParamQuantity(FOOD_PARAM)->setDisplayValue(mapped);
         foodCV = mapped - params[FOOD_PARAM].getValue();
     } else {
-        getParamQuantity(FOOD_PARAM)->setDisplayValue(params[FOOD_PARAM].getValue());
         foodCV = 0.f;
     }
 }
